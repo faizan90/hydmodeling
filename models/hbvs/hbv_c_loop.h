48923ae1b42e134aadb64887572dec18c1f73cb9
Faizan_Uni <faizananwar2006@gmail.com>
2019-04-15 09:14:31 +0200

    Organized hbv_c_loop

--- models/hbvs/hbv_c_loop.h
+++ models/hbvs/hbv_c_loop.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <math.h>
 #include <stdio.h>
-//#include <time.h>
 
 typedef double DT_D;
 typedef long DT_UL;
@@ -10,6 +9,7 @@ inline DT_D min(DT_D a, DT_D b) {
     if (a <= b) {
         return a;
     }
+
     else {
         return b;
     }
@@ -19,6 +19,7 @@ inline DT_D max(DT_D a, DT_D b) {
     if (a >= b) {
         return a;
     }
+
     else {
         return b;
     }
@@ -43,7 +44,7 @@ DT_D hbv_c_loop(
     ) {
 
 	/*
-	 * implementation of hbv_loop in c
+	 * Implementation of distributed hbv_loop in cpp
 	 */
 
 	size_t i, j, k, m, n, p, cur_p, out_inc_n;
@@ -64,11 +65,11 @@ DT_D hbv_c_loop(
 	// prms
 	DT_D tt, cm, pwp, fc, beta, k_uu, ur_thr, k_ul, k_d, k_ll;
 
-	// previous current cell and time arrays
+	// Previous current cell and time arrays
 	DT_D *temp_j_arr, *prec_j_arr, *petn_j_arr, *prms_j_arr;
 	DT_D *outs_j_arr;
 
-	// indicies of variables in the outs_arr
+	// Indicies of variables in the outs_arr
 	snow_i = 0;
 	lppt_i = 1;
 	somo_i = 2;
@@ -80,7 +81,7 @@ DT_D hbv_c_loop(
 	ur_lr_i = 8;
 	lr_i = 9;
 
-	// indicies of variables in the prms_arr
+	// Indicies of variables in the prms_arr
 	tt_i = 0;
 	cm_i = 1;
 	p_cm_i = 2;
@@ -101,7 +102,7 @@ DT_D hbv_c_loop(
 		out_inc_n = ((*n_time_steps) + 1) * *n_vars_outs_arr;
 	}
 
-	// assign initial values
+	// Assign initial values
 	for (i = 0, j = 0;
 		 j < (*n_cells * out_inc_n);
 		 i = (i + 4), j = (j + out_inc_n)) {
@@ -111,8 +112,6 @@ DT_D hbv_c_loop(
 		outs_arr[j + ur_i] = inis_arr[i + 2];
 		outs_arr[j + lr_i] = inis_arr[i + 3];
 	}
-
-//	clock_t start = clock(), diff;
 
 	for (i = 0, j = 0, k = 0, m = 0;
 		 j < (*n_cells * *n_time_steps);
@@ -147,6 +146,8 @@ DT_D hbv_c_loop(
              n < *n_time_steps;
              ++n, p = (p + *n_vars_outs_arr)) {
 
+        	// In case of optimization overwrite one vector. This is to save
+        	// memory.
         	if (*opt_flag) {
         		cur_p = 0;
         	}
@@ -159,63 +160,69 @@ DT_D hbv_c_loop(
             prec = prec_j_arr[n];
             petn = petn_j_arr[n];
 
-            // snow and liquid ppt
+            // Snow and liquid ppt
             if (temp < tt) {
                 outs_j_arr[cur_p + snow_i] = pre_snow + prec;
                 outs_j_arr[cur_p + lppt_i] = 0.0;
             }
+
             else {
                 snow_melt = ((cm + (p_cm * prec)) * (temp - tt));
             	outs_j_arr[cur_p + snow_i] = max(0.0,  pre_snow - snow_melt);
                 outs_j_arr[cur_p + lppt_i] = prec + min(pre_snow, snow_melt);
             }
+
             pre_snow = outs_j_arr[cur_p + snow_i];
             lppt = outs_j_arr[cur_p + lppt_i];
 
-            // soil moisture and ET
-			//if rel_fc_beta goes above 1 i.e. pre_somo > fc, that is self-
-			//corrected in the next step by reducing the sm and giving that
-			//water to runoff.
+            // Soil moisture and ET
+			// If rel_fc_beta goes above 1 i.e. pre_somo > fc, that is self-
+			// corrected in the next step by reducing the sm and giving that
+			// water to runoff.
             rel_fc_beta = pow((pre_somo / fc), beta);
             avail_somo = pre_somo + (lppt * (1 - rel_fc_beta));
 
     		if (pre_somo > pwp) {
     			outs_j_arr[cur_p + evtn_i] = min(avail_somo, petn);
     		}
+
     		else {
     			outs_j_arr[cur_p + evtn_i] = min(
     					avail_somo, (pre_somo / fc) * petn);
     		}
 
-			//sometimes somo goes slightly below 0 for certain parameters'
-			//combinations, also this will allow for drought modelling
-			//without causing problems.
-			//comparison with 0.0 for rounding errors in case avail_somo and
-			//outs_j_arr[i, evtn_i] are almost equal
+			// Sometimes somo goes slightly below 0 for certain parameters'
+			// combinations, also this will allow for drought modelling
+			// without causing problems.
+			// Comparison with 0.0 for rounding errors in case avail_somo and
+			// outs_j_arr[i, evtn_i] are almost equal
             outs_j_arr[cur_p + somo_i] =  max(
                     0.0, avail_somo - outs_j_arr[cur_p + evtn_i]);
 
             pre_somo = outs_j_arr[cur_p + somo_i];
 
-            // total runoff
+            // Total runoff
             outs_j_arr[cur_p + rnof_i] = lppt * rel_fc_beta;
 
-            // runonff, upper reservoir, upper outlet
+            // Runonff, upper reservoir, upper outlet
             outs_j_arr[cur_p + ur_uo_i] = (
             	max(0.0, (pre_ur_sto - ur_thr) * k_uu));
+
             ur_uo_rnof = outs_j_arr[cur_p + ur_uo_i];
 
-            // runoff, upper reservoir, lower outlet
+            // Runoff, upper reservoir, lower outlet
             outs_j_arr[cur_p + ur_lo_i] = (
             	max(0.0, (pre_ur_sto - ur_uo_rnof) * k_ul));
+
             ur_lo_rnof = outs_j_arr[cur_p + ur_lo_i];
 
-            // seepage to groundwater
+            // Seepage to groundwater
             outs_j_arr[cur_p + ur_lr_i] = (
             	max(0.0, (pre_ur_sto - ur_uo_rnof - ur_lo_rnof) * k_d));
+
             ur_lr_seep = outs_j_arr[cur_p + ur_lr_i];
 
-            // upper reservoir storage
+            // Upper reservoir storage
             outs_j_arr[cur_p + ur_i] = (
             	max(0.0,
             		(pre_ur_sto -
@@ -223,22 +230,21 @@ DT_D hbv_c_loop(
 					 ur_lo_rnof -
 					 ur_lr_seep +
 					 outs_j_arr[cur_p + rnof_i])));
+
             pre_ur_sto = outs_j_arr[cur_p + ur_i];
 
-            // lower reservoir runoff and storage
+            // Lower reservoir runoff and storage
             lr_rnof = pre_lr_sto * k_ll;
+
             outs_j_arr[cur_p + lr_i] = (pre_lr_sto + ur_lr_seep - lr_rnof);
+
             pre_lr_sto = outs_j_arr[cur_p + lr_i];
 
-            // upper and lower reservoirs combined discharge
+            // Upper and lower reservoirs combined discharge
             qsim_arr[n] += (rnof_q_conv[0] * cell_area *
 							(ur_uo_rnof + ur_lo_rnof + lr_rnof));
         }
 	}
-
-//	diff = clock() - start;
-//	int msec = diff * 1000 / CLOCKS_PER_SEC;
-//	printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
 
 	return 0.0;
 }
